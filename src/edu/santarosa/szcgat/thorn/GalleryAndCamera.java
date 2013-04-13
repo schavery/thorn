cc93306d116e69c28aebb7b253ceb864ae625d71
Steve Avery <schavery@gmail.com>
2013-04-13 14:01:26 -0700

    initial commit

---
+++ src/edu/santarosa/szcgat/thorn/GalleryAndCamera.java
@@ -0,0 +1,158 @@
+package edu.santarosa.szcgat.thorn;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GalleryAndCamera extends FragmentActivity implements
+		ActionBar.TabListener {
+
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a
+	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+	 * will keep every loaded fragment in memory. If this becomes too memory
+	 * intensive, it may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	SectionsPagerAdapter mSectionsPagerAdapter;
+
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	ViewPager mViewPager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gallery_and_camera);
+
+		// Set up the action bar.
+		final ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the app.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(
+				getSupportFragmentManager());
+
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+
+		actionBar.addTab(actionBar.newTab().setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setTabListener(this));
+
+		mViewPager.setCurrentItem(1);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+		// When the given tab is selected, switch to the corresponding page in
+		// the ViewPager.
+		mViewPager.setCurrentItem(tab.getPosition());
+		
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+	}
+
+	/**
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the sections/tabs/pages.
+	 */
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			Fragment fragment;
+			if (position == 0) {
+				fragment = new CameraFragment();
+			}
+			else {
+				fragment = new GalleryFragment();
+			}
+
+			return fragment;
+		}
+
+		@Override
+		public int getCount() {
+			return 2;
+		}
+
+	}
+
+	public static class GalleryFragment extends Fragment {
+
+		public GalleryFragment() {
+
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_gallery,
+					container, false);
+
+			return rootView;
+		}
+	}
+
+	public static class CameraFragment extends Fragment {
+
+		public CameraFragment() {
+
+		}
+
+		@Override
+		public void onResume() {
+			super.onResume();
+			Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+			intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10); // needs to be dynamic
+			GalleryAndCamera.CameraFragment.this.startActivityForResult(intent, 0);
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
+			return rootView;
+		}
+	}
+
+	public void openProfile(View v) {
+		GalleryAndCamera.this.startActivity(new Intent(GalleryAndCamera.this,
+				ProfileAndCamera.class));
+	}
+
+}
---
+++ src/edu/santarosa/szcgat/thorn/Processor.java
@@ -0,0 +1,14 @@
+package edu.santarosa.szcgat.thorn;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Processor extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+	
+}
